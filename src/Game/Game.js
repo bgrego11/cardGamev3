177e54f7fc731015a4c0ef92a74c8da39d12007c
StevenS125 <sts12589@gmail.com>
2019-01-08 21:03:10 -0500

    added some functionality

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 import decks from '../data/decks';
 
+import './Game.css'
+
 export default class Game extends Component {
+  
   constructor(props) {
     super(props)
     this.state ={
@@ -11,12 +14,13 @@ export default class Game extends Component {
       blackDiscard: [],
       whiteDiscard: [],
       cards: [],
-      players: [{name: "stevio", cardsInHand: []}],
+      players: [{name: "No Current Players", cardsInHand: []}],
       names:["zeus","steve","rebecca"],
       dealer: "",
     }
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    
   }
 
   componentWillMount() {
@@ -49,60 +53,77 @@ handleChange(e) {
 
 
 dealCards = () => {
-        let cardArray = this.state.whiteCards;
+    
+  
+  let cardArray = this.state.whiteCards;
         let nameArray = this.state.players[0].cardsInHand
         const max = this.state.whiteCards.length-1;
   
         let numSelect = Math.floor(Math.random() * max);
       nameArray.push(this.state.whiteCards[numSelect])
   
-      let newWhite = cardArray.filter((hero) => 
-        hero !== this.state.whiteCards[numSelect]);
-
-        
+      let newWhite = 
+      cardArray.filter((hero) => 
+        hero !== this.state.whiteCards[numSelect])
+  
         this.setState({
         whiteCards: newWhite,
         players: [{
           name: "stevio",
           cardsInHand: nameArray,
-          points: 0},
-          {
-            name: "cheerio",
-            cardsInHand: nameArray,
-            points: 0},
-            {
-              name: "dope",
-              cardsInHand: nameArray,
-              points: 0}]
+          points: 0,
+        dealer: false},
+        {
+          name: "reggie",
+          cardsInHand: nameArray,
+          points: 0,
+        dealer: false},
+        {
+          name: "craig",
+          cardsInHand: nameArray,
+          points: 0,
+        dealer: false}]
       })
     }
 
+    singleShuffle = () => {
+      const firstMap = this.state.players.map(i => i)
+    console.log(firstMap)
+    }
+   
+
 showWhite = () => {
+
   if (this.state.players[0].cardsInHand.length < 7) {
    this.dealCards()
 
-    if(this.state.players[0].cardsInHand.length < 7) {
-      this.showWhite() }
+setTimeout(() => {
+
+   if (this.state.players[0].cardsInHand.length < 7) {
+      this.showWhite()                                    
   }
+}, 200);
+    }
 }
   render() {
     const currentPlayers = this.state.players.map((i, index) => <div key={index}>
-                                                                        <div>{i.name}</div>
-                                                                        <div>{i.cards}</div>
+                                                                        <div className="playerName">{i.name}</div>
                                                                         </div>)
     const currentCards = 
     this.state.players.map((i, index) => <ul key={index}>{i.cardsInHand}</ul>)
     
+
+
     return (
       <div className="container">
+      <div className="heroGame">
   <h1>Number of players</h1>
-  <h2>{this.state.players[0].cardsInHand}</h2>
+  <div>{this.state.players[0].name === "No Current Players" ? 0 : this.state.players.length }</div>
+  <div>current players</div>
+  <div>{currentPlayers}</div>
+  <button onClick={this.showWhite}>Start Game</button>
+  </div>
 
-      <button onClick={this.dealCards}>shuffle</button>
-      <div>current players</div>
-      <div>{currentPlayers}</div>
-      <div>{currentCards}</div>
-      <button onClick={this.showWhite}>shuffle</button>
       </div>
     );
   }
