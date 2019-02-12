fbdabd94d0f6abbbc9a62f8b6cb788bab5a3f140
StevenS125 <sts12589@gmail.com>
2019-02-12 11:44:49 -0500

    added production build to deploy to heroku

---
+++ build/precache-manifest.7f19bf0796225f52ba7f38f9b35d987c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "85912286150975d2b813",
+    "url": "/static/js/main.85912286.chunk.js"
+  },
+  {
+    "revision": "c16ce3adea2720d7d7e4",
+    "url": "/static/js/1.c16ce3ad.chunk.js"
+  },
+  {
+    "revision": "85912286150975d2b813",
+    "url": "/static/css/main.f6942172.chunk.css"
+  },
+  {
+    "revision": "c16ce3adea2720d7d7e4",
+    "url": "/static/css/1.739438c7.chunk.css"
+  },
+  {
+    "revision": "fe7146d73701e7c0ae46bc0fa5126347",
+    "url": "/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1974dfaa9358b46c00f260bf65cebe7d.js"
+  "/precache-manifest.7f19bf0796225f52ba7f38f9b35d987c.js"
 );
 
 workbox.clientsClaim();
