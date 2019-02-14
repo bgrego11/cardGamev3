f386035e7d3880c14571f0de443fbf14f88c4583
bgrego11 <natureboy4271@gmail.com>
2019-02-13 23:25:27 -0500

    stupid

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'snydz.auth0.com',
   clientId: 'ZoKU0RUGkyPh5aigHdeq7IC841Fp9rUY',
-  callbackUrl: 'http://cardsapplication.herokuapp.com//callback'
+  callbackUrl: 'http://cardsapplication.herokuapp.com/callback'
 }
