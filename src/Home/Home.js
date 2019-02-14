48b9cee6070144ff13e694c8da1c234df8c037de
StevenS125 <sts12589@gmail.com>
2019-02-14 12:38:36 -0500

    console socket

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -23,8 +23,11 @@ class Home extends Component {
   
 
       componentWillMount() {
+        
         const socket = io.connect("https://cardsapplication.herokuapp.com", { secure: true, reconnect: true, rejectUnauthorized: false})
-  
+        
+        console.log(socket);
+
         const addUserName = async () => { const res = await axios.get('wild-rice-5480.auth0.com/userinfo', { headers: {"Authorization" : `Bearer ${localStorage.access_token}`}})
         return await res.data;
 }
