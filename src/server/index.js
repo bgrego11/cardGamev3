c99b9a6045d50d1ba1064f5dbaa16aa667733b9d
StevenS125 <sts12589@gmail.com>
2019-02-14 16:09:34 -0500

    express change

--- src/server/index.js
+++ src/server/index.js
@@ -1,52 +1,70 @@
 const express = require('express');
-const cors = require('cors');
-const app = express();
-const server = require('http').Server(app);
-const io = module.exports.io = require('socket.io')(server, { origins: '*:*'});
+const socketIO = require('socket.io');
+const path = require('path');
 
-const PORT = process.env.PORT || 3231
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname + '/../../build');
 
-const SocketManager = require('./socketmanager')
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use(cors())
-
-app.use(express.static(__dirname + '/../../build'))
+const io = socketIO(server);
 
 io.on('connection', SocketManager)
 console.log(io)
 
-server.listen(PORT, ()=>{
-	console.log("Connected to port:" + PORT);
-})
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://heroku_2x8x1pf5:77rq0ebp3pidpukjth60bu8sq1@ds115214.mlab.com:15214/heroku_2x8x1pf5', {useNewUrlParser: true});
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+// const server = require('http').Server(app);
+// const io = module.exports.io = require('socket.io')(server, { origins: '*:*'});
 
-const Game = mongoose.model('Game', 
-{
-    blackCards: Array,
-    whiteCards: Array,
-    cards: Array,
-    players: Array,
-    names:Array,
-    dealer: String,
-    cardsinplay: Array,
-    score: Array
-}
-);
 
-let game = new Game({ 
-    blackCards: ["card 1","card 2"],
-    whiteCards: ["card 1","card 2"],
-    cards: ["card 1","card 2"],
-    players: ["player 1","player 2"],
-    names:["player 1","player 2"],
-    dealer: "ben",
-    cardsinplay: ["player 1","player 2"],
-    score: ["player 1","player 2"]
-});
-game.save(function (err) {
-  if (err) return handleError(err);
-  console.log("SAVED")
-  // saved!
-});
+// const PORT = process.env.PORT || 3231
+
+// const SocketManager = require('./socketmanager')
+
+// app.use(cors())
+
+// app.use(express.static(__dirname + '/../../build'))
+
+// io.on('connection', SocketManager)
+// console.log(io)
+
+// server.listen(PORT, ()=>{
+// 	console.log("Connected to port:" + PORT);
+// })
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://heroku_2x8x1pf5:77rq0ebp3pidpukjth60bu8sq1@ds115214.mlab.com:15214/heroku_2x8x1pf5', {useNewUrlParser: true});
+
+// const Game = mongoose.model('Game', 
+// {
+//     blackCards: Array,
+//     whiteCards: Array,
+//     cards: Array,
+//     players: Array,
+//     names:Array,
+//     dealer: String,
+//     cardsinplay: Array,
+//     score: Array
+// }
+// );
+
+// let game = new Game({ 
+//     blackCards: ["card 1","card 2"],
+//     whiteCards: ["card 1","card 2"],
+//     cards: ["card 1","card 2"],
+//     players: ["player 1","player 2"],
+//     names:["player 1","player 2"],
+//     dealer: "ben",
+//     cardsinplay: ["player 1","player 2"],
+//     score: ["player 1","player 2"]
+// });
+// game.save(function (err) {
+//   if (err) return handleError(err);
+//   console.log("SAVED")
+//   // saved!
+// });
