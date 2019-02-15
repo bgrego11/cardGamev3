14314ca5900a13b79f0e17cd82f5da6e0f4fbe57
StevenS125 <sts12589@gmail.com>
2019-02-15 11:56:35 -0500

    changed route, server not loading auth

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Redirect, Route, Router } from 'react-router-dom';
 import App from './App';
-import Profile from './Profile/Profile';
 import Home from './Home/Home';
 import Callback from './Callback/Callback';
 import Auth from './Auth/Auth';
@@ -23,10 +22,6 @@ export const makeMainRoutes = () => {
           <Route path="/home" render={(props) => (
             !auth.isAuthenticated() ? (<Redirect to="/"/>) : (
             <Home auth={auth} {...props} />
-            ))} />
-            <Route path="/profile" render={(props) => (
-            !auth.isAuthenticated() ? (<Redirect to="/"/>) : (
-            <Profile auth={auth} {...props} />
             ))} />
           <Route path="/callback" render={(props) => {
             handleAuthentication(props);
